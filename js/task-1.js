5e8f758b7148fd4975b3880e0d24c5f5b403138c
Marensfc <alexeymaren12@gmail.com>
2024-01-16 17:26:51 +0200

    Made 1 task

---
+++ js/task-1.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const listElements = document.querySelectorAll('#categories>.item')
+
+console.log('Number of categories:', listElements.length);
+
+listElements.forEach((elem, index) => {
+    console.log('category:', elem.firstElementChild.textContent);
+    console.log('Elements:', elem.lastElementChild.children.length);
+})
