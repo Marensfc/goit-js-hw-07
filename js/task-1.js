f14feb6e044a66182e585da9354886e04cfb7538
Marensfc <alexeymaren12@gmail.com>
2024-01-17 23:36:19 +0200

    Made 2 task

--- js/task-1.js
+++ js/task-1.js
@@ -4,7 +4,7 @@ const listElements = document.querySelectorAll('#categories>.item')
 
 console.log('Number of categories:', listElements.length);
 
-listElements.forEach((elem, index) => {
+listElements.forEach(elem => {
     console.log('category:', elem.firstElementChild.textContent);
     console.log('Elements:', elem.lastElementChild.children.length);
 })
