8f24a3318ae40af8ab737a48c3b146162b7caa1c
Marensfc <alexeymaren12@gmail.com>
2024-01-18 16:22:10 +0200

    Made 5 task

--- js/task-5.js
+++ js/task-5.js
@@ -1,5 +1,16 @@
+"use strict";
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const color = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", (event) => {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = getRandomHexColor();
+});
