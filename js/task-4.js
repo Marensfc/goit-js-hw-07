5fa2b7b2b913144f1ca3d0a93c9b579af82ca955
Marensfc <alexeymaren12@gmail.com>
2024-01-18 15:44:35 +0200

    Made task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,23 @@
+"use strict";
+
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (
+    form.elements.email.value.trim().length === 0 ||
+    form.elements.password.value.trim().length === 0
+  ) {
+    alert("All form fields must be filled in");
+  } else {
+    const userData = {
+      [form.elements.email.name]: form.elements.email.value.trim(),
+      [form.elements.password.name]: form.elements.password.value.trim(),
+    };
+    console.log(userData);
+    form.reset();
+  }
+});
+
 
